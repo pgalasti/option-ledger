3eb55d5aa23b0acd5f1eb959b0730b62a800e6d2
Paul Galasti <pgalasti@gmail.com>
2025-12-04 12:35:30 -0500

    Moved to own file

---
+++ src/model/CloseType.js
@@ -0,0 +1,7 @@
+export const CloseState = {
+    EXPIRED:           'Expired',
+    EXPIRED_WORTHLESS: 'Expired Worthless'
+    ROLLED:            'Rolled',
+    BUY_TO_CLOSE:      'Buy to Close',
+    ASSIGNED:          'Assigned
+};
--- src/model/TradeType.js
+++ src/model/TradeType.js
@@ -2,11 +2,3 @@ export const TradeType = {
     COVERED_CALL: 'Covered Call',
     SHORT_PUT:    'Short Put'
 };
-
-export const CloseState = {
-    EXPIRED:           'Expired',
-    EXPIRED_WORTHLESS: 'Expired Worthless'
-    ROLLED:            'Rolled',
-    BUY_TO_CLOSE:      'Buy to Close',
-    ASSIGNED:          'Assigned
-};
