949a6e18d48445fd67717d34f0fcab615fe5a4d6
Paul Galasti <pgalasti@gmail.com>
2025-12-04 12:34:40 -0500

    Added closed type

--- src/model/TradeType.js
+++ src/model/TradeType.js
@@ -1,4 +1,12 @@
 export const TradeType = {
     COVERED_CALL: 'Covered Call',
-    SHORT_PUT: 'Short Put'
+    SHORT_PUT:    'Short Put'
+};
+
+export const CloseState = {
+    EXPIRED:           'Expired',
+    EXPIRED_WORTHLESS: 'Expired Worthless'
+    ROLLED:            'Rolled',
+    BUY_TO_CLOSE:      'Buy to Close',
+    ASSIGNED:          'Assigned
 };
